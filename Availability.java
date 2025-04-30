f37d5996818aac960f74a7914a291f99ccc71dcf
elbakri-elbashir-ozu <147049682+MohamedElbashir10@users.noreply.github.com>
2025-04-30 13:22:01 +0300

    Availability class updated

--- Availability.java
+++ Availability.java
@@ -1,21 +1,25 @@
-public class Availabiltiy;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Availability {
   private LocalDate date;
   private LocalTime startTime;
   private LocalTime endTime;
 
-  public Availablity(LocalDate date, LocalTime startTime, LocalTime endTime){
-      this.date=date;
-      this.startTime=startTime;
-      this.endTime=endTime;
+  public Availability(LocalDate date, LocalTime startTime, LocalTime endTime) {
+    this.date = date;
+    this.startTime = startTime;
+    this.endTime = endTime;
   }
 
-  public void addAvailability(){
+  public void addAvailability() {
   }
 
-  public void displayAvailability(){
-    system.out.println.("Availability Details: ");
-    system.out.println.("Date: " + date);
-    system.out.println.("Start Time: " + startTime);
-    system.out.println.("End Time: " + endTime);
+  public void displayAvailability() {
+    System.out.println("Availability Details: ");
+    System.out.println("Date: " + date);
+    System.out.println("Start Time: " + startTime);
+    System.out.println("End Time: " + endTime);
   }
 
+}
