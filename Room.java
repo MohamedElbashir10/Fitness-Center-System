79a50b076b615892f4fe9c86f2c7b4451bcce051
elbakri-elbashir-ozu <147049682+MohamedElbashir10@users.noreply.github.com>
2025-04-30 13:11:17 +0300

    Room class updated

--- Room.java
+++ Room.java
@@ -1,13 +1,48 @@
 public class Room {
 
     private String name;
+    private int roomId;
+    private int capacity;
+    private String description;
 
-    public Room(String name){
+    public Room(String name,int roomId, int capacity, String description){
         this.name = name;
+        this.roomId = roomId;
+        this.capacity = capacity;
+        this.description = description;
     }
 
     public void addRoom(){
 
     }
+
+    public void updateRoom(){
+
+    }
+
+    public void deleteRoom(){
+
+    }
+
+    public String getRoomDetails() {
+        return "ID: " + roomId + " | Name: " + name + " | Capacity: " + capacity + " | Description: " + description;
+    }
+
+
+    // Getters
+    public int getId() {
+        return roomId;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getCapacity() {
+        return capacity;
+    }
+    public String getDescription() {
+        return description;
+    }
+
+
 
 }
