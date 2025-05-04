eeba03de7702bf0d4666fa3c05f0d504ad253153
iremard <107619686+iremard@users.noreply.github.com>
2025-05-04 12:30:36 +0300

    NotificationService.java

--- NotificationService.java
+++ NotificationService.java
@@ -8,12 +8,27 @@ public class NotificationService {
     system.out.println( " Message : " + message);
     system.out.println( " ------------------");
 }
+public static void sendInAppNotification(String userId, String message) {
+        System.out.println("IN-APP NOTIFICATION");
+        System.out.println("User ID : " + userId);
+        System.out.println("Message : " + message);
+        System.out.println("--------------------------------------");
+
+        JOptionPane.showMessageDialog(null,
+                "Notification for user: " + userId + "\n" + message,
+                "In-App Notification",
+                JOptionPane.INFORMATION_MESSAGE);
+}
 public static void sendBookingConfirmation( String email, String sessionInfo){
   String subject = " Booking Confirmation ";
   String message = " Your session has been booked succesfully : \n" + sessionInfo;
   sendEmailNotification(email, subject, message);
 }
-
+public static void sendSessionReminder(String email, String sessionInfo) {
+        String subject = "Session Reminder";
+        String message = "This is a reminder for your upcoming session:\n" + sessionInfo;
+        sendEmailNotification(email, subject, message);
+}
 public static void sendCancellationNotification(String email, String sessionInfo){
   String subject= "Sesssion Cancelled";
   String message= "The following session has been cancelled:\n" + sessionInfo;
@@ -22,18 +37,6 @@ public static void sendCancellationNotification(String email, String sessionInfo
 public static void sendSystemMessage(String email, String userId, String subject, String message)
   sendEmailNotification(email, subject, message);
   sendAppNotification(userId, message);
-}
-public static void sendInAppNotification(String userId, String message) {
-        System.out.println("IN-APP NOTIFICATION");
-        System.out.println("User ID : " + userId);
-        System.out.println("Message : " + message);
-        System.out.println("--------------------------------------");
-
-       
-        JOptionPane.showMessageDialog(null,
-                "Notification for user: " + userId + "\n" + message,
-                "In-App Notification",
-                JOptionPane.INFORMATION_MESSAGE);
 }
 
 }
