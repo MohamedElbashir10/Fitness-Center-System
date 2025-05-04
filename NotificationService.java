672ec23365b84b6668fd6f0173fc71b2516cc2e7
iremard <107619686+iremard@users.noreply.github.com>
2025-05-04 12:24:08 +0300

    NotificationService.java

---
+++ NotificationService.java
@@ -0,0 +1,40 @@
+import javax.swing. JOptionPane;
+
+public class NotificationService {
+  public static void sendEmailNotification(String to, String subject, String message)
+    system.out.println(" EMAIL NOTIFICATION " );
+    system.out.println( " TO : " + to);
+    system.out.println( " Subject : " + subject);
+    system.out.println( " Message : " + message);
+    system.out.println( " ------------------");
+}
+public static void sendBookingConfirmation( String email, String sessionInfo){
+  String subject = " Booking Confirmation ";
+  String message = " Your session has been booked succesfully : \n" + sessionInfo;
+  sendEmailNotification(email, subject, message);
+}
+
+public static void sendCancellationNotification(String email, String sessionInfo){
+  String subject= "Sesssion Cancelled";
+  String message= "The following session has been cancelled:\n" + sessionInfo;
+  sendEmailNotification(email, subject, message);
+}
+public static void sendSystemMessage(String email, String userId, String subject, String message)
+  sendEmailNotification(email, subject, message);
+  sendAppNotification(userId, message);
+}
+public static void sendInAppNotification(String userId, String message) {
+        System.out.println("IN-APP NOTIFICATION");
+        System.out.println("User ID : " + userId);
+        System.out.println("Message : " + message);
+        System.out.println("--------------------------------------");
+
+       
+        JOptionPane.showMessageDialog(null,
+                "Notification for user: " + userId + "\n" + message,
+                "In-App Notification",
+                JOptionPane.INFORMATION_MESSAGE);
+}
+
+}
+  
