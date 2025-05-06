72f0bcdbc0657c5cdc4d532afe63c12e5e09627f
Mohamed Elayadi <mohamed.elayadi@ozu.edu.tr>
2025-05-06 19:43:52 +0300

    Adding A workout sessions list, method for reserving sessions and a getter for the list of workouts reserved

--- Member.java
+++ Member.java
@@ -1,8 +1,23 @@
+import java.util.List;
+
 public class Member extends User {
     
-    //Construtor
+    // List of sessions the member has reserved
+    private List<WorkoutSession> reservedSessions; 
+
+    // Construtor
     public Member(int id, String name, String username, String password) {
         super(id, name, username, password, "Member");
+    }
+
+    // Reserving a session method
+    public void reserveSession(WorkoutSession session) {
+        reservedSessions.add(session);
+    }
+
+    // ReservedSession by the member Getter
+    public List<WorkoutSession> getReservedSessions() {
+        return reservedSessions;
     }
 
 }
