6f279ecf7af7b29e2aff0a7c8d1399bbe7539ec8
Mohamed Elayadi <mohamed.elayadi@ozu.edu.tr>
2025-05-06 19:48:43 +0300

    adding arraylists to handle the assigned sessions and available time slots for the trainer

--- Trainer.java
+++ Trainer.java
@@ -1,9 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Trainer extends User {
+    
+    // Sessions assigned to this trainer
+    private List<WorkoutSession> assignedSessions; 
+
+    // Trainer's available time slots
+    private List<Availability> availabilitySlots;  
 
     // Constructor
     public Trainer(int id, String name, String username, String password) {
         super(id, name, username, password, "Trainer");
+        this.assignedSessions = new ArrayList<>();
+        this.availabilitySlots = new ArrayList<>();
     }
 
-    
+
 }
