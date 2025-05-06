e310a9f980fc424ed7342951cc77152805156136
Mohamed Elayadi <mohamed.elayadi@ozu.edu.tr>
2025-05-06 19:38:10 +0300

    Updating the details of user class and adding getters and setters

--- User.java
+++ User.java
@@ -1,12 +1,60 @@
 public class User {
-    String name;
-    String email;
-    String password;
 
-    public User(String name, String email, String password) {
+    private int id;
+    private String name;
+    private String username;
+    private String password;
+    private String role; // "Member", "Trainer", "Admin"
+
+
+    // Constructor
+    public User(int id, String name, String username, String password, String role) {
+        this.id = id;
         this.name = name;
-        this.email = email;
+        this.username = username;
         this.password = password;
+        this.role = role;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
     }
 
 }
