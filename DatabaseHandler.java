a946a74d5e215211edefe8ce4c048ce1f34b3f52
Mohamed Elayadi <mohamed.elayadi@ozu.edu.tr>
2025-05-09 15:33:28 +0300

    Implemenet sample databaseHandler

---
+++ DatabaseHandler.java
@@ -0,0 +1,23 @@
+import java.sql.*;
+
+public class FCSDatabase {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3307/fcs_db";
+        String user = "root";
+        String password = "rootpassword";
+
+        try (Connection conn = DriverManager.getConnection(url, user, password)) {
+            System.out.println("Connected to the database!");
+
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM users");
+
+            while (rs.next()) {
+                System.out.println(rs.getString("name") + " (" + rs.getString("role") + ")");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
