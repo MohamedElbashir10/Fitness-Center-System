9256d51ac554cab76da396aa4ffb2bd82a3258cc
Mohamed Elayadi <mohamed.elayadi@ozu.edu.tr>
2025-05-11 18:32:43 +0300

    constructor

--- SessionBookingCoordinator.java
+++ SessionBookingCoordinator.java
@@ -1,5 +1,22 @@
+import java.util.List;
+import java.util.Scanner;
 
 public class SessionBookingCoordinator {
 
+    private List<Trainer> trainers;
+    private List<Room> rooms;
+    private List<WorkoutSession> sessions;
+    private Availability availability;
+    private Reservation reservationService;
 
+    public SessionBookingCoordinator(List<Trainer> trainers, List<Room> rooms, List<WorkoutSession> sessions, Availability availability, Reservation reservationService) {
+        
+        this.trainers = trainers;
+        this.rooms = rooms;
+        this.sessions = sessions;
+        this.availability = availability;
+        this.reservationService = reservationService;
+    }
+
+    
 }
